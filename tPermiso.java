da233b1ff76b91ac08aff3575fa73de0864940d3
JavierGR99 <48418909+JavierGR99@users.noreply.github.com>
2019-12-08 21:47:05 +0100

    Add files via upload

---
+++ tPermiso.java
@@ -0,0 +1,72 @@
+package Tablas;
+
+import java.io.Serializable;
+import java.lang.String;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: tPermiso
+ *
+ */
+@Entity
+
+public class tPermiso implements Serializable {
+
+	   
+	@Id
+	private tRol rolName;   
+	@Id
+	private String pantalla;
+	private boolean acceso;
+	private boolean insertar;
+	private boolean modificar;
+	private boolean borrar;
+	private static final long serialVersionUID = 1L;
+
+	public tPermiso() {
+		super();
+	}   
+	public tRol getRolName() {
+		return this.rolName;
+	}
+
+	public void setRolName(tRol rolName) {
+		this.rolName = rolName;
+	}   
+	public String getPantalla() {
+		return this.pantalla;
+	}
+
+	public void setPantalla(String pantalla) {
+		this.pantalla = pantalla;
+	}   
+	public boolean getAcceso() {
+		return this.acceso;
+	}
+
+	public void setAcceso(boolean acceso) {
+		this.acceso = acceso;
+	}   
+	public boolean getInsertar() {
+		return this.insertar;
+	}
+
+	public void setInsertar(boolean insertar) {
+		this.insertar = insertar;
+	}   
+	public boolean getModificar() {
+		return this.modificar;
+	}
+
+	public void setModificar(boolean modificar) {
+		this.modificar = modificar;
+	}   
+	public boolean getBorrar() {
+		return this.borrar;
+	}
+
+	public void setBorrar(boolean borrar) {
+		this.borrar = borrar;
+	}
+   
+}
